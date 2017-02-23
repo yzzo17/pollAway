89f5ce454b8d3405bee3f472d280f91f06289c0d
javif89 <javif89@Little-Walrus>
2017-02-23 13:04:26 -0500

    Added all the framework for the new classes

---
+++ src/dossier/Poll.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dossier;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author javif89
+ */
+public class Poll {
+    ArrayList<Question> questions;
+    ArrayList<ArrayList<Answer>> results;
+    
+    /*
+    TODO: Get and set and other poll functionality
+    */
+}
---
+++ src/dossier/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dossier;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author javif89
+ */
+public class User {
+    ArrayList<Demographic> demographics;
+    String id; //????
+    /*
+    TODO:
+    Add get and set methods for demographics
+    */
+}
